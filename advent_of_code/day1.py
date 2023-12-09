b715ea0b8a561b7c6d8d7181d96285d656d4dff0
Thorvald Molthe Ballestad <thorvald@simula.no>
2023-12-09 17:24:01 +0100

    Day 1 complete

--- advent_of_code/day1.py
+++ advent_of_code/day1.py
@@ -1,47 +1,90 @@
-import re
+"""Way too overcomplicated solution using a search over Trie trees."""
+import functools
+from typing import Hashable, Iterable
 
 from advent_of_code.util import get_input_text
 
-def _first_digit(string: str) -> str:
-    return next(filter(str.isdigit, string))
 
-def _first_digit_or_written_digit(string: str) -> str:
-    all_digit_words = {'zero': '0',
-     'one': '1',
-     'two': '2',
-     'three': '3',
-     'four': '4',
-     'five': '5',
-     'six': '6',
-     'seven': '7',
-     'eight': '8',
-     'nine': '9'}
+def generate_trie_tree(words: Iterable[Iterable[Hashable]]) -> dict:
+    """Generate a trie tree structure from a collection of strings.
 
-    all_digits = (str(i) for i in range(10))
-    regex = f"^.*?(({"|".join((*all_digit_words, *all_digits))}))"
-    pattern = re.compile(regex)
-    digit = pattern.match(string).group(1)
-    
-    digit = all_digit_words.get(digit, digit)
-    return digit
-    
+    See https://en.wikipedia.org/wiki/Trie for background.
+    """
+
+    tree = {}
+    for word in words:
+        functools.reduce(lambda base, el: base.setdefault(el, {}), word, tree)
+    return tree
 
 
-def first_and_last_digit_combination(string: str, digit_finder=_first_digit_or_written_digit) -> int:
-    return int(
-            digit_finder(string)
-            + digit_finder(str(reversed(string))),
-            )
+class SequenceNotInTree(Exception):
+    """The sequence is not in the tree."""
+
+    ...
+
+
+def search_tree(tree, sequence):
+    node = tree
+    for i, element in enumerate(sequence):
+        try:
+            node = node[element]
+        except KeyError:
+            raise SequenceNotInTree
+        if not node:  # Empty node => leaf node
+            return sequence[: i + 1]
+
+    raise SequenceNotInTree
+
+
+def get_string_to_value():
+    all_digit_words = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+    digits = {str(i): str(i) for i in range(10)}
+    return all_digit_words | digits
+
+
+def get_trie_tree_for_numbers():
+    return generate_trie_tree(get_string_to_value())
+
+
+def find_occurence(tree, sequence):
+    for start_position in range(len(sequence)):
+        try:
+            answer = search_tree(tree, sequence[start_position:])
+        except SequenceNotInTree:
+            pass
+        else:
+            return answer
+    raise SequenceNotInTree
 
 
 def main():
     input_text = get_input_text(1)
     lines = input_text.strip().split("\n")
+
+    string_to_value = get_string_to_value()
+    tree = generate_trie_tree(get_string_to_value())
+    reverse_tree = generate_trie_tree(
+        (reversed(element) for element in get_string_to_value())
+    )
+
     answer = sum(
-            first_and_last_digit_combination(line) for line in lines
-            )
+        int(string_to_value[find_occurence(tree, line)]
+        + string_to_value[find_occurence(reverse_tree, line[::-1])[::-1]])
+        for line in lines
+    )
     print(answer)
+
 
 if __name__ == "__main__":
     main()
-
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,19 +1,46 @@
 import pytest
-from advent_of_code.day1 import first_and_last_digit_combination, _first_digit_or_written_digit
-
-@pytest.mark.parametrize("string, expected_out",[
-    ("12", 12),
-    ("foo1bar2baz", 12),
-    ("1", 11),
-    ])
-def test_first_and_last_digit(string, expected_out):
-    assert first_and_last_digit_combination(string) == expected_out
+from advent_of_code.day1 import (
+    SequenceNotInTree,
+    find_occurence,
+    generate_trie_tree,
+    get_trie_tree_for_numbers,
+    search_tree,
+)
 
 
-@pytest.mark.parametrize("string, expected_out",[
-    ("12", "1"),
-    ("onefoo1bar2baz", "1"),
-    ("ztwo1", "2"),
-    ])
-def test_first_digit_or_written_digit(string, expected_out):
-    assert _first_digit_or_written_digit(string) == expected_out
+@pytest.mark.parametrize(
+    "words, expected_tree",
+    [
+        (
+            ("foo", "bar", "fot"),
+            {"f": {"o": {"o": {}, "t": {}}}, "b": {"a": {"r": {}}}},
+        ),
+        (("1", "2"), {"1": {}, "2": {}}),
+    ],
+)
+def test_generate_tri_tree(words: list[str], expected_tree: dict):
+    assert generate_trie_tree(words) == expected_tree
+
+
+def test_search_tree():
+    assert search_tree(
+        {"1": {}, "2": {}},
+        "1",
+        ) == "1"
+
+    assert search_tree(
+        {"1": {"2": {}}, "2": {}},
+        "123",
+        ) == "12"
+
+    with pytest.raises(SequenceNotInTree):
+        search_tree(
+            {"1": {"2": {}}, "2": {}},
+            "0123",
+            )
+
+def test_tree_search():
+    tree = get_trie_tree_for_numbers()
+    assert find_occurence(
+        tree, "jaone3two"
+    ) == "one"
