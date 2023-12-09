652a59b245d2f160ed535546648cd79990ba6f03
Thorvald Molthe Ballestad <thorvald@simula.no>
2023-12-09 14:53:48 +0100

    Can't remember what I did...

--- advent_of_code/day1.py
+++ advent_of_code/day1.py
@@ -1,13 +1,36 @@
+import re
 
 from advent_of_code.util import get_input_text
 
 def _first_digit(string: str) -> str:
     return next(filter(str.isdigit, string))
 
-def first_and_last_digit_combination(string: str) -> int:
+def _first_digit_or_written_digit(string: str) -> str:
+    all_digit_words = {'zero': '0',
+     'one': '1',
+     'two': '2',
+     'three': '3',
+     'four': '4',
+     'five': '5',
+     'six': '6',
+     'seven': '7',
+     'eight': '8',
+     'nine': '9'}
+
+    all_digits = (str(i) for i in range(10))
+    regex = f"^.*?(({"|".join((*all_digit_words, *all_digits))}))"
+    pattern = re.compile(regex)
+    digit = pattern.match(string).group(1)
+    
+    digit = all_digit_words.get(digit, digit)
+    return digit
+    
+
+
+def first_and_last_digit_combination(string: str, digit_finder=_first_digit_or_written_digit) -> int:
     return int(
-            _first_digit(string)
-            + _first_digit(reversed(string)),
+            digit_finder(string)
+            + digit_finder(str(reversed(string))),
             )
 
 
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,5 +1,5 @@
 import pytest
-from advent_of_code.day1 import first_and_last_digit_combination
+from advent_of_code.day1 import first_and_last_digit_combination, _first_digit_or_written_digit
 
 @pytest.mark.parametrize("string, expected_out",[
     ("12", 12),
@@ -8,3 +8,12 @@ from advent_of_code.day1 import first_and_last_digit_combination
     ])
 def test_first_and_last_digit(string, expected_out):
     assert first_and_last_digit_combination(string) == expected_out
+
+
+@pytest.mark.parametrize("string, expected_out",[
+    ("12", "1"),
+    ("onefoo1bar2baz", "1"),
+    ("ztwo1", "2"),
+    ])
+def test_first_digit_or_written_digit(string, expected_out):
+    assert _first_digit_or_written_digit(string) == expected_out
