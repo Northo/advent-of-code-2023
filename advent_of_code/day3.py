bd8a8fc61585c6bfe9cd1ec16eebd3d9df839be6
Thorvald Molthe Ballestad <thorvald@simula.no>
2023-12-09 18:37:17 +0100

    --wip-- [skip ci]

---
+++ advent_of_code/day3.py
@@ -0,0 +1,44 @@
+import itertools
+import pandas as pd
+
+
+def line_to_records(line: str) -> list[dict]:
+    """Extract all the objects on a line."""
+
+    parts = line.split(".")
+    character_counter = 0
+    objects = []
+
+    for part in parts:
+        if part:  # do not add empty elements (Only dots)
+            objects.append(
+                {
+                    "element": part,
+                    "start_pos": character_counter,
+                    "end_pos": character_counter + len(part) - 1,
+                }
+            )
+        character_counter += len(part) + 1  # Add one to take into account the .
+    return objects
+
+
+def lines_to_records(lines: str):
+    wide_form_records = []
+    for i, line in enumerate(lines):
+        for record in line_to_records(line):
+            wide_form_records.append(record | {"line": i})
+
+    # Make it long-form
+    long_form_records = []
+    for element_id, record in enumerate(wide_form_records):
+        element_width = 1 + record["end_pos"] - record["start_pos"]
+        for i in range(element_width):
+            long_form_records.append({
+                "element_id": element_id,
+                "line": record["line"],
+                "pos": record["start_pos"] + i,
+                "element": record["element"],
+            })
+    return long_form_records
+
+# TODO per element that is not a number, search for neighbour element id's
---
+++ tests/test_day3.py
@@ -0,0 +1,44 @@
+import pytest
+
+from advent_of_code.day3 import line_to_records, lines_to_records
+
+
+@pytest.mark.parametrize(
+    "line, records",
+    [
+        (
+            "467..114..",
+            [
+                {"element": "467", "start_pos": 0, "end_pos": 2},
+                {"element": "114", "start_pos": 5, "end_pos": 7},
+            ],
+        ),
+        (
+            "467....*..",
+            [
+                {"element": "467", "start_pos": 0, "end_pos": 2},
+                {"element": "*", "start_pos": 7, "end_pos": 7},
+            ],
+        ),
+    ],
+)
+def test_line_to_records(line: str, records: list[dict]):
+    assert line_to_records(line) == records
+
+
+def test_lines_to_records():
+    lines = ["467..114..", "467....*.."]
+    records = lines_to_records(lines)
+
+    assert records == [
+        {"element_id": 0, "pos": 0, "line": 0, "element": "467"},
+        {"element_id": 0, "pos": 1, "line": 0, "element": "467"},
+        {"element_id": 0, "pos": 2, "line": 0, "element": "467"},
+        {"element_id": 1, "pos": 5, "line": 0, "element": "114"},
+        {"element_id": 1, "pos": 6, "line": 0, "element": "114"},
+        {"element_id": 1, "pos": 7, "line": 0, "element": "114"},
+        {"element_id": 2, "pos": 0, "line": 1, "element": "467"},
+        {"element_id": 2, "pos": 1, "line": 1, "element": "467"},
+        {"element_id": 2, "pos": 2, "line": 1, "element": "467"},
+        {"element_id": 3, "pos": 7, "line": 1, "element": "*"},
+    ]
