51fe239b887d12039e946441032a7ffdd664b099
Thorvald Molthe Ballestad <thorvald@simula.no>
2023-12-17 16:24:34 +0100

    Cookiecut with https://github.com/radix-ai/poetry-cookiecutter

---
+++ tests/test_import.py
@@ -0,0 +1,8 @@
+"""Test advent-of-code."""
+
+import advent_of_code
+
+
+def test_import() -> None:
+    """Test that the package can be imported."""
+    assert isinstance(advent_of_code.__name__, str)
