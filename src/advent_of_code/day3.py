234d2070f9785a5f47db63bef67bd53fda6e997d
Thorvald Molthe Ballestad <thorvald@simula.no>
2023-12-19 17:54:42 +0100

    Complete day3 part 1

--- src/advent_of_code/day3.py
+++ src/advent_of_code/day3.py
@@ -1,5 +1,10 @@
+import itertools
 import re
 from typing import Any
+
+import pandas as pd
+
+from advent_of_code.util import get_input_text
 
 
 def line_to_records(line: str) -> list[dict]:
@@ -56,6 +61,33 @@ def get_neighbouring_positions(pos: int, line: int) -> list[tuple[int, int]]:
 
 def main():
     lines = get_input_text(3).strip().split("\n")
-    records = lines_to_records(lines)
+    records = pd.DataFrame(lines_to_records(lines))
+    records["is_number"] = records["element"].str.isnumeric()
+    records = records.set_index(["pos", "line"])
 
-    pos_to_element = {(record["pos"], record["line"]): record["element_id"] for record in records}
+    # For all the non-numeric entries, find the relevant numbers
+    part_positions = records.query("~is_number").index.to_list()
+    relevant_neigbouring_positions = itertools.chain.from_iterable(
+        get_neighbouring_positions(*position) for position in part_positions
+    )
+    existing_neigbouring_positions = list(
+        set(relevant_neigbouring_positions) & set(records.query("is_number").index)
+    )
+
+    element_ids = records.loc[existing_neigbouring_positions].element_id.unique()
+
+    # We now have all the element_ids, time to get the element number and return it
+    numeric_element_values = (
+        records.query("is_number")[["element_id", "element"]]
+        .drop_duplicates()
+        .set_index("element_id")["element"]
+        .astype(int)
+        .to_dict()
+    )
+
+    answer = sum(numeric_element_values[id] for id in element_ids)
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
